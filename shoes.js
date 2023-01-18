7c02cbfcc281af27cebd9d93ec22805216b7c6f4
kaushal Pathak <kaushaldu017@gmail.com>
2023-01-19 01:40:48 +0530

    shoe_page

---
+++ shoes.js
@@ -0,0 +1,319 @@
+let item=[{image:"https://images.ctfassets.net/5de70he6op10/7hE2nHay4uPG8CfR2VjyLD/72fef4a91b5d09b45ec62e03f4697dd5/060922_30offSelectSwim_OutCatTile_copy_2.jpg?w=546&q=80&fm=webp",
+   name:"",
+   price:"",
+   size:"",
+   catagory:"",
+   img:"",
+
+
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/60058286_029_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Fisher man sport sandal",
+   price:125,
+   size:5,
+   catagory: "Bestselling",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/65972366_067_s?fit=constrain&hei=56&qlt=75")',
+    'url( "https://images.urbndata.com/is/image/Anthropologie/65972366_040_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/60385945_009_b3?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Seycheele Bits N' Pieces Wovens Flats",
+   price:99,
+   size:6,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/65972374_000_s?fit=constrain&hei=56&qlt=75")']
+
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/65368508_001_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Pilcro Twisted Leather Slides",
+   price:130,
+   size:5,
+   catagory: "Bestselling",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/69266591_001_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69266591_010_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69266591_040_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69266591_070_s?fit=constrain&hei=56&qlt=75")',
+	'url("https://images.urbndata.com/is/image/Anthropologie/69266591_072_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/64998826_012_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Guilhrmina Square-Toe Ballet Flats",
+   price:120,
+   size:5,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/69267698_001_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69267698_072_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69267698_070_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/68829134_024_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Guilhrmina Pointed-Toe Mary Janes",
+   price:140,
+   size:6,
+   catagory: "Bestselling",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/69267698_001_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69267698_072_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69267698_070_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/63060966_016_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Kelsi Dagger Brooklyn Lens Suede Loafers",
+   price:130,
+   size:7,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/76183573_010_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/68947142_071_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Angel Alarcon Patent-Lather Square-Toe Flats",
+   price:150,
+   size:7,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/76183573_010_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/67426239_066_b16?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Angel Alarcon Strappy Heels",
+   price:150,
+   size:6,
+   catagory: "Bestselling",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/69267698_001_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69267698_072_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69267698_070_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/68774272_085_b14?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"CAVERLER Maize Heels",
+   price:185,
+   size:7,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/78001815_040_s?fit=constrain&hei=56&qlt=75")',
+        'url("https://images.urbndata.com/is/image/Anthropologie/78001815_066_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/68774272_085_b14?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Nicenza Mettalic Mules",
+   price:160,
+   size:7,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/76183573_010_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/68542190_066_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Bibi lou Aoi Heels",
+   price:180,
+   size:6,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/69266591_001_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69266591_010_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69266591_040_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/69266591_070_s?fit=constrain&hei=56&qlt=75")',
+	'url("https://images.urbndata.com/is/image/Anthropologie/69266591_072_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/66043233_055_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"K-swiss Classic Sneakers",
+   price:70,
+   size:5,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/78001815_040_s?fit=constrain&hei=56&qlt=75")',
+        'url("https://images.urbndata.com/is/image/Anthropologie/78001815_066_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/67264093_010_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Faguo Ceiba Baskets Sneakers",
+   price:135,
+   size:5,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/61389706_060_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/61389706_270_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/66976606_001_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Matisse Jelly Slide Sandals",
+   price:28,
+   size:6,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/78001815_040_s?fit=constrain&hei=56&qlt=75")',
+        'url("https://images.urbndata.com/is/image/Anthropologie/78001815_066_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/67128207_015_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Laidback London Daini Sandals",
+   price:68,
+   size:6,
+   catagory: "Bestselling",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/61389706_060_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/61389706_270_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/67665521_015_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Woven Platform Tie-up Sandals",
+   price:112,
+   size:5,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/76183573_010_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/67105452_059_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Melissa Possesion Degradde Flats",
+   price:48,
+   size:6,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/61389706_060_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/61389706_270_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/67454595_001_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Tie-up Thang Sandals",
+   price:77,
+   size:5,
+   catagory: "Bestselling",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/61389706_060_s?fit=constrain&hei=56&qlt=75")',
+    'url("https://images.urbndata.com/is/image/Anthropologie/61389706_270_s?fit=constrain&hei=56&qlt=75")'],
+   },
+   {image:"https://images.urbndata.com/is/image/Anthropologie/67563486_034_b2?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+   name:"Transport Zebra Sandals",
+   price:98,
+   size:6,
+   catagory: "Newest",
+   img: ['url("https://images.urbndata.com/is/image/Anthropologie/78001815_040_s?fit=constrain&hei=56&qlt=75")',
+        'url("https://images.urbndata.com/is/image/Anthropologie/78001815_066_s?fit=constrain&hei=56&qlt=75")'],
+   }
+   ] ;
+   let items=JSON.parse(localStorage.getItem("data"))
+
+ document.querySelector("#sort").addEventListener("change", on1)
+ function on1(){
+    let select=document.querySelector("#sort").value
+    
+
+    if(select=="lowhigh"){
+        item.sort(function(a,b){
+        
+            return a.price-b.price
+        })
+        display(item);
+    }else if(select=="highlow"){
+        item.sort(function(a,b){
+            return b.price-a.price
+        })
+        display(item);
+    }
+else if(select=="az"){
+        item.sort(function(a,b){
+    if(a.name>b.name) return 1
+    if(a.name<b.name) return -1
+    else return 0
+         })
+         display(item);
+        }else if(select=="za"){
+        item.sort(function(a,b){
+    if(a.name<b.name) return 1
+    if(a.name>b.name) return -1
+    else return 0
+         })
+         display(item)
+    }else if(select=="shl"){
+        item.sort(function(a,b){
+            if(a.size<b.size) return 1
+            if(a.size>b.size) return -1
+            else return 0
+        })
+        display(item)
+    }else if(select=="slh"){
+        item.sort(function(a,b){
+            if(a.size>b.size) return 1
+            if(a.size<b.size) return -1
+            else return 0
+        })
+        display(item)
+    }else if(select=="Newest"){
+        output=item.filter(function(el){
+            return(el.catagory=="Newest")
+        })
+        display(output)
+    }else if(select=="Bestselling"){
+        output=item.filter(function(el){
+            return(el.catagory=="Bestselling")
+        })
+        display(output)
+    }else if(select=="Featured"){
+        window.location.reload()
+    }
+
+    // window.location.reload()
+
+}
+display1(items)
+ display(item)
+
+ 
+ function display(item){
+   document.querySelector("#images").innerHTML=""
+   item.forEach(function(el){
+    let box=document.createElement("div")
+    box.addEventListener("click", function(){
+        send(el)
+    })
+    let im=document.createElement("img")
+    
+    im.setAttribute("src",el.image)
+    let nam=document.createElement("p")
+    nam.innerText=el.name
+    // if(item.price==""){
+    let pr=document.createElement("p")
+    if(el.price!=""){
+    pr.innerText=`$ ${el.price}`
+    }
+    let sz=document.createElement("p")
+    let colr=document.createElement("p")
+     for(let i=0;i<el.img.length;i++){
+        colr.setAttribute("class","coloursForPic")
+        if(i<4){
+        let inp=document.createElement("button")
+        //let a=
+       // console.log(a)
+      
+        inp.style.backgroundImage =el.img[i]
+    
+    
+        
+       
+       colr.append(inp)
+        }else
+        colr.append("+"+(el.img.length-4))
+     }    
+    
+    box.append(im,nam,pr,sz,colr)
+    document.querySelector("#images").append(box)
+
+   })
+   localStorage.setItem("data", JSON.stringify(item))
+ }
+ function display1(items){
+
+   items.forEach(function(el){
+    let box=document.createElement("div")
+    let im=document.createElement("img")
+    im.setAttribute("src",el.image)
+    let nam=document.createElement("p")
+    nam.innerText=el.name
+    let pr=document.createElement("p")
+    pr.innerText=`$ ${el.price}`
+    let sz=document.createElement("p")
+    // sz.innerText=el.size
+    box.append(im,nam,pr,sz)
+    document.querySelector("#images").append(box)
+
+   })
+   localStorage.setItem("data",JSON.stringify(item))
+ }
+//  document.querySelector("#btn-btn").addEventListener("click", on2)
+//  function on2(){
+   
+//  }
+//  }
+function send(el){
+    localStorage.setItem("obj", JSON.stringify(el))
+    window.location.href="cartshoe.html"
+}
+ document.querySelector("#price").addEventListener("click", pri)
+ function pri(){
+    item.sort(function(a,b){
+        return a.price-b.price
+    })
+    display(item)
+ }
+ document.querySelector("#size").addEventListener("click", siz)
+ function siz(){
+    item.sort(function(a,b){
+        return a.size-b.size
+    })
+    display(item)
+ }
+ 
